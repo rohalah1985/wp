280aec4863ce73ff03b6820df74c90b15cfb4638
Rohalah <rohalah8282@gmail.com>
2024-08-25 16:45:03 +0330

    start blog wordpress

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang=" <?php language_attributes() ?> ">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/font-awesome.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css" />
+
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="top-bar">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="data">
+              <span><i class="fa fa-phone-square"></i> 09176501189</span>
+              <span><i class="fa fa-map-marker"></i> جهرم - خیابان جانبازان غربی</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-menu">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="menu-items">
+              <ul>
+                <li><a href="">خانه</a></li>
+                <li><a href="">اخبار</a></li>
+                <li><a href="">تماس با ما</a></li>
+                <li>
+                  <a href="">درباره ما</a>
+                  <ul>
+                    <li><a href="">خانه</a></li>
+                    <li><a href="">اخبار</a></li>
+                    <li>
+                      <a href="">تماس با ما</a>
+                      <ul>
+                        <li><a href="">خانه</a></li>
+                        <li><a href="">اخبار</a></li>
+                        <li><a href="">تماس با ما</a></li>
+                        <li><a href="">درباره ما</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="">درباره ما</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="logo-box"></div>
+          </div>
+        </div>
+      </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,281 @@
+<?php get_header() ?>
+
+    <div class="slider-box text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1>دوره طراحی سایت</h1>
+            <p>دوره کامل آموزش برنامه نویسی فرانت اند</p>
+            <a href="#">بیشتر بخوانید <i class="fa fa-long-arrow-left"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="desc">
+              <h4>دوره وردپرسی شو</h4>
+              <span>دوره کامل آموزش وردپرس</span>
+              <p>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
+                جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را
+                برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در
+                زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و
+                دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و
+                زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+                پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+              </p>
+              <div class="text-start">
+                <a href="#" class="site-btn">توضیحات بیشتر</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-8">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="item">
+                  <i class="fa fa-gears"></i>
+                  <h4>آموزش</h4>
+                  <p>
+                    ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و
+                    با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه
+                    و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+                    تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود
+                  </p>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="item">
+                  <i class="fa fa-github-square"></i>
+                  <h4>آموزش</h4>
+                  <p>
+                    ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و
+                    با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه
+                    و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+                    تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود
+                  </p>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="item">
+                  <i class="fa fa-bookmark-o"></i>
+                  <h4>آموزش</h4>
+                  <p>
+                    ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و
+                    با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه
+                    و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+                    تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود
+                  </p>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="item">
+                  <i class="fa faّ-phone"></i>
+                  <h4>آموزش</h4>
+                  <p>
+                    ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و
+                    با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه
+                    و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+                    تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="index-blog">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="description">
+              <h4>عنوان ها در این قسمت باشد</h4>
+              <div class="meta">
+                <span><i class="fa fa-calendar-o"></i>1403/5/27</span>
+                <span><i class="fa fa-comment-o"></i>10 نظر</spaنظرn>
+                  <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود</p>
+                  <div class="text-start">
+                    <a href="#" class="site-btn">ادامه مطلب...</a>
+                  </div>
+              </div>
+            </div> 
+          </div>
+          <div class="col-md-6">
+            <div class="pic">
+              <img src="<?php echo get_template_directory_uri() ?>/img/custom_search-min.png" alt="">
+
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+       
+          <div class="col-md-6">
+            <div class="pic">
+              <img src="<?php echo get_template_directory_uri() ?>/img/wordpress_sidebars-min.png" alt="">
+
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="description">
+              <h4>عنوان ها در این قسمت باشد</h4>
+              <div class="meta">
+                <span><i class="fa fa-calendar-o"></i>1403/5/27</span>
+                <span><i class="fa fa-comment-o"></i>10 نظر</spaنظرn>
+                  <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود</p>
+                  <div class="text-start">
+                    <a href="#" class="site-btn">ادامه مطلب...</a>
+                  </div>
+              </div>
+            </div> 
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="description">
+              <h4>عنوان ها در این قسمت باشد</h4>
+              <div class="meta">
+                <span><i class="fa fa-calendar-o"></i>1403/5/27</span>
+                <span><i class="fa fa-comment-o"></i>10 نظر</spaنظرn>
+                  <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود</p>
+                  <div class="text-start">
+                    <a href="#" class="site-btn">ادامه مطلب...</a>
+                  </div>
+              </div>
+            </div> 
+          </div>
+          <div class="col-md-6">
+            <div class="pic">
+              <img src="<?php echo get_template_directory_uri() ?>/img/css_acardion_menu-min.png" alt="">
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+<div class="site_content">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="box">
+          <span class="title">مطالب پربازدید</span>
+          <div class="d-flex post_item_bx">
+            <div class="col-md-5">
+              <div class="pic">
+                <img src="<?php echo get_template_directory_uri() ?>/img/css_acardion_menu-min.png" alt="">
+              </div>
+            </div>
+            <div class="col-md-7">
+              <div class="desc2">
+                <h5>عنوان مطلب در این قسمت ، آموزش وردپرس</h5>
+                <p>عنوان مطلب در این قسمت ، آموزش وردپرس نوان مطلب در این قسمت ، آموزش وردپرس </p>
+                <span><i class="fa fa-calendar-o"></i>1403.5.28</span>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex post_item_bx">
+            <div class="col-md-5">
+              <div class="pic">
+                <img src="<?php echo get_template_directory_uri() ?>/img/wordpress_sidebars-min.png" alt="">
+              </div>
+            </div>
+            <div class="col-md-7">
+              <div class="desc2">
+                <h5>عنوان مطلب در این قسمت ، آموزش وردپرس</h5>
+                <p>عنوان مطلب در این قسمت ، آموزش وردپرس نوان مطلب در این قسمت ، آموزش وردپرس </p>
+                <span><i class="fa fa-calendar-o"></i>1403.5.28</span>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex post_item_bx">
+            <div class="col-md-5">
+              <div class="pic">
+                <img src="<?php echo get_template_directory_uri() ?>/img/custom_search-min.png" alt="">
+              </div>
+            </div>
+            <div class="col-md-7">
+              <div class="desc2">
+                <h5>عنوان مطلب در این قسمت ، آموزش وردپرس</h5>
+                <p>عنوان مطلب در این قسمت ، آموزش وردپرس نوان مطلب در این قسمت ، آموزش وردپرس </p>
+                <span><i class="fa fa-calendar-o"></i>1403.5.28</span>
+              </div>
+            </div>
+          </div>
+          
+      
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <div class="box">
+          <span class="title">آموزش وردپرس  </span>
+          <div class="d-flex post_item_bx">
+            <div class="col-md-5">
+              <div class="pic">
+                <img src="<?php echo get_template_directory_uri() ?>/img/custom_search-min.png" alt="">
+              </div>
+            </div>
+            <div class="col-md-7">
+              <div class="desc2">
+                <h5>عنوان مطلب در این قسمت ، آموزش وردپرس</h5>
+                <p>عنوان مطلب در این قسمت ، آموزش وردپرس نوان مطلب در این قسمت ، آموزش وردپرس </p>
+                <span><i class="fa fa-calendar-o"></i>1403.5.28</span>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex post_item_bx">
+            <div class="col-md-5">
+              <div class="pic">
+                <img src="<?php echo get_template_directory_uri() ?>/img/css_acardion_menu-min.png" alt="">
+              </div>
+            </div>
+            <div class="col-md-7">
+              <div class="desc2">
+                <h5>عنوان مطلب در این قسمت ، آموزش وردپرس</h5>
+                <p>عنوان مطلب در این قسمت ، آموزش وردپرس نوان مطلب در این قسمت ، آموزش وردپرس </p>
+                <span><i class="fa fa-calendar-o"></i>1403.5.28</span>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex post_item_bx">
+            <div class="col-md-5">
+              <div class="pic">
+                <img src="<?php echo get_template_directory_uri() ?>/img/wordpress_sidebars-min.png" alt="">
+              </div>
+            </div>
+            <div class="col-md-7">
+              <div class="desc2">
+                <h5>عنوان مطلب در این قسمت ، آموزش وردپرس</h5>
+                <p>عنوان مطلب در این قسمت ، آموزش وردپرس نوان مطلب در این قسمت ، آموزش وردپرس </p>
+                <span><i class="fa fa-calendar-o"></i>1403.5.28</span>
+              </div>
+            </div>
+          </div>
+          
+      
+        </div>
+      </div>
+    </div>
+
+    
+  </div>
+
+  </div>
+
+<?php get_footer() ; ?>
